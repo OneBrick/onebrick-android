e97dee498e3e3e223b44287f06213ce1fb457663
Prakash Joshi <pjoshi@twitter.com>
2014-10-11 12:15:33 -0700

    REST Client setup

---
+++ OneBrick/app/src/main/java/org/onebrick/android/OneBrickApplication.java
@@ -0,0 +1,31 @@
+package org.onebrick.android;
+
+import android.content.Context;
+
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+
+public class OneBrickApplication extends com.activeandroid.app.Application {
+    private static final String TAG = "OneBrickApplication";
+
+    private static Context context;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        OneBrickApplication.context = this;
+
+        // Create global configuration and initialize ImageLoader with this configuration
+        DisplayImageOptions defaultOptions = new DisplayImageOptions.Builder().
+                cacheInMemory(true).cacheOnDisk(true).build();
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(getApplicationContext())
+                .defaultDisplayImageOptions(defaultOptions)
+                .build();
+        ImageLoader.getInstance().init(config);
+    }
+
+    public static OneBrickClient getRestClient() {
+        return (OneBrickClient) OneBrickClient.getInstance(OneBrickClient.class, OneBrickApplication.context);
+    }
+}
