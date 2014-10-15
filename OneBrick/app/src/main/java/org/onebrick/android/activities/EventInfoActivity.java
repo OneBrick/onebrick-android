d1b4bc3243b90658371f8605557e3ed058094ad9
Ashwin Gv <gv.ashwin@outlook.com>
2014-10-15 15:26:53 -0700

    Calling the endpoint to get event detailed info and updating the views

--- OneBrick/app/src/main/java/org/onebrick/android/activities/EventInfoActivity.java
+++ OneBrick/app/src/main/java/org/onebrick/android/activities/EventInfoActivity.java
@@ -1,17 +1,76 @@
 package org.onebrick.android.activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.apache.http.Header;
+import org.json.JSONObject;
+import org.onebrick.android.OneBrickApplication;
+import org.onebrick.android.OneBrickClient;
 import org.onebrick.android.R;
+import org.onebrick.android.models.Event;
 
 public class EventInfoActivity extends Activity {
+    String eventId;
+    private static final String TAG = HomeActivity.class.getName().toString();
+    OneBrickClient obClient = OneBrickApplication.getRestClient();
+    JsonHttpResponseHandler responseHandler = new JsonHttpResponseHandler(){
+        @Override
+        public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+            super.onSuccess(statusCode, headers, response);
+            Log.i("TAG","Success"+response.toString());
+            Event updatedEvent = Event.getUpdatedEvent(response);
+            updateViews(updatedEvent);
+        }
+
+        @Override
+        public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+            Log.i("TAG","Json Request to fetch event info failed");
+            super.onFailure(statusCode, headers, throwable, errorResponse);
+        }
+
+        @Override
+        public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+            Log.i("TAG","FAIL "+responseString);
+            super.onFailure(statusCode, headers, responseString, throwable);
+        }
+
+    };
+
+    TextView tvEventName;
+    TextView tvEventDateTime;
+    TextView tvEventBrief;
+    TextView tvEventLocation;
+
+    private void updateViews(Event updatedEvent) {
+        tvEventName.setText(updatedEvent.getTitle());
+        tvEventDateTime.setText(updatedEvent.getEventStartDate()
+                +" to "
+                +updatedEvent.getEventEndDate());
+        tvEventBrief.setText(updatedEvent.getEventDescription());
+        tvEventLocation.setText(updatedEvent.getEventAddress());
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_info);
+        tvEventName = (TextView) findViewById(R.id.tvEventName);
+        tvEventDateTime = (TextView) findViewById(R.id.tvEventTime);
+        tvEventBrief = (TextView) findViewById(R.id.tvEventBrief);
+        tvEventLocation = (TextView) findViewById(R.id.tvEventLocation);
+        Intent eventInfo = getIntent();
+        eventId = eventInfo.getStringExtra("EventId");
+        getActionBar().setTitle("Event Info");
+        //Toast.makeText(this,"The Event id to display is"+eventId,Toast.LENGTH_SHORT).show();
+        obClient.getEventInfo(eventId, responseHandler);
     }
 
 
