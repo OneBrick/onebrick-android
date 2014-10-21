23b85d5e7cfc9a486ef4ba60cec879fb4d336e63
Ashwin Gv <gv.ashwin@outlook.com>
2014-10-21 13:34:23 -0700

    +Display notification receiver

---
+++ OneBrick/app/src/main/java/org/onebrick/android/activities/DisplayNotificationReceiver.java
@@ -0,0 +1,39 @@
+package org.onebrick.android.activities;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+
+import org.onebrick.android.R;
+
+/**
+ * Created by AshwinGV on 10/21/14.
+ */
+public class DisplayNotificationReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Bundle args = intent.getExtras();
+        String eventName = args.getString("EventName");
+        String message = args.getString("DisplayMessage");
+        NotificationManager notificationManager = (NotificationManager)
+                context.getSystemService(context.NOTIFICATION_SERVICE);
+        Intent i = new Intent(context.getApplicationContext(),SplashScreenActivity.class);
+        PendingIntent pIntent = PendingIntent.getActivity(context, 0, i, 0);
+
+        // build notification
+        // the addAction re-use the same intent to keep the example short
+        Notification mBuilder  = new NotificationCompat.Builder(context)
+                .setSmallIcon(R.drawable.ic_launcher)
+                .setContentTitle(""+eventName)
+                .setContentText(""+message)
+                .setContentIntent(pIntent)
+                .setAutoCancel(true)
+                .build();
+        notificationManager.notify(0, mBuilder);
+    }
+}
