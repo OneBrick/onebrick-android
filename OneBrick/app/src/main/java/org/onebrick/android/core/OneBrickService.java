d1d4819582ccd3f000897e3c609ebbb04ac51fc2
Prakash Joshi <prakashjoshi0101@gmail.com>
2015-05-02 18:00:22 -0700

    added missing API endpoints

--- OneBrick/app/src/main/java/org/onebrick/android/core/OneBrickService.java
+++ OneBrick/app/src/main/java/org/onebrick/android/core/OneBrickService.java
@@ -6,7 +6,13 @@ import org.onebrick.android.models.Event;
 import java.util.List;
 import java.util.Map;
 
+import retrofit.client.Response;
+import retrofit.http.Field;
+import retrofit.http.FormUrlEncoded;
 import retrofit.http.GET;
+import retrofit.http.POST;
+import retrofit.http.PUT;
+import retrofit.http.Path;
 import retrofit.http.Query;
 
 public interface OneBrickService {
@@ -15,4 +21,27 @@ public interface OneBrickService {
 
     @GET("/event.json")
     List<Event> getAllEvents(@Query("chapter") int chapterId);
+
+    @GET("/event/{eventId}.json")
+    Response eventInfo(@Query("ukey") String ukey, @Path("eventId") int eventId);
+
+//    @POST("/user/login.json")
+//    @FormUrlEncoded
+//    Response login(@Field(""));
+
+    @POST("/event/{eventId}/rsvp.json")
+    Response rsvp(@Query("ukey") String ukey, @Path("eventId") int eventId);
+
+    @PUT("/event/{eventId}/unrsvp.json")
+    Response unrsvp(@Query("ukey") String ukey);
+
+    /**
+     *
+     * @param includePastEvents 1 or 0
+     */
+    @GET("/event.json")
+    Response myEvents(@Query("ukey") String ukey, @Query("includePastEvents") int includePastEvents);
+
+    @GET("/event.json")
+    Response search(@Query("chapter") int chapterId, @Query("search") String search);
 }
